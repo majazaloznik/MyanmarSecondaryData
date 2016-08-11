21df83b32bcb69303fea3e4a9e57543b0ec778b7
majazaloznik <maja.zaloznik@gmail.com>
2016-08-11 16:16:48 +0100

    Initial Commit

---
+++ 01-Mapping-Import-CleanUp.R
@@ -0,0 +1,8 @@
+###############################################################################
+## MAPPING
+###############################################################################
+
+## shape file source: DIVA GIS www.diva-gis.org
+## Country: Myanmar
+## Subject: Administrative areas (GADM)
+## http://biogeo.ucdavis.edu/data/diva/adm/MMR_adm.zip - downloaded 11.8.2016
